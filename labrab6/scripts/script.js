e1b7a6e8d075525b44b36d1ce3376e82c50944d9
TbotaPhantA <botakiller@mail.ru>
2024-02-11 21:51:49 +0000

    loading indicator was added + profile information was added using javascript

--- labrab6/scripts/script.js
+++ labrab6/scripts/script.js
@@ -1,11 +1,37 @@
 window.addEventListener('DOMContentLoaded', function() {
-    // Hide a loading indicator
+    const mainSection = document.getElementById('main-section')
+    const servicesSection = document.getElementById('services');
+    const contactSection = document.getElementById('contact');
+    if (mainSection) { mainSection.style.display = 'none'; }
+    if (servicesSection) { servicesSection.style.display = 'none'; }
+    if (contactSection) { contactSection.style.display = 'none'; }
+    // Show a loading indicator
     document.getElementById('loading-indicator').style.display = 'block';
-    document.getElementById('main-section').style.display = 'none';
 });
 
 window.addEventListener('load', function() {
-    // Show the loading indicator
+    // Hide the loading indicator
     document.getElementById('loading-indicator').style.display = 'none';
-    document.getElementById('main-section').style.display = 'block';
+    const mainSection = document.getElementById('main-section');
+    const servicesSection = document.getElementById('services');
+    const contactSection = document.getElementById('contact');
+    if (mainSection) { mainSection.style.display = 'block'; }
+    if (servicesSection) { servicesSection.style.display = 'block'; }
+    if (contactSection) { contactSection.style.display = 'block'; }
 });
+
+function getUserInfo() {
+    return {
+        name: "Иванов Иван Иванович",
+        photoUrl: "https://cdn-icons-png.flaticon.com/512/5556/5556468.png"
+    };
+}
+
+const userInfo = getUserInfo();
+
+const usernameElement = document.getElementById("username");
+usernameElement.textContent = userInfo.name;
+
+const photoElement = document.getElementById("user-photo");
+photoElement.src = userInfo.photoUrl;
+
